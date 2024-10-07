a45f65fbe648e5c31fa58726022aec67406f30c8
Felipe <143225058+Felipe572@users.noreply.github.com>
2024-10-07 20:32:15 -0300

    Atualizando JS

---
+++ js/functions.js
@@ -0,0 +1,7 @@
+const screenOne = document.querySelector('.screenOne')
+const screenTwo = document.querySelector('.screenTwo') 
+
+function toggleScreen() {
+    screenOne.classList.toggle('hide')
+    screenTwo.classList.toggle('hide')
+}
